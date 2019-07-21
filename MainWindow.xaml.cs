c8ff76b14553c10fe4e65cac302794bc8355cc02
Muhammad Ibnuh <ibnuhdev@gmail.com>
2019-07-21 09:20:21 +0700

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace UTClock
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary
+    public partial class MainWindow : Window
+    {
+        private readonly DispatcherTimer _timer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _timer = new DispatcherTimer { Interval = TimeSpan.FromTicks(0) };
+            _timer.Tick += ClockTicking;
+            _timer.Start();
+        }
+
+        private void ClockTicking(object sender, EventArgs e)
+        {
+            TitleText.Text = DateTime.UtcNow.ToString();
+        }
+    }
+}
